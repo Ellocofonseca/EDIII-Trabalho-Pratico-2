6b1814c0efb8eaa214614bfd59b7ff88de6f0254
MiguelRF <fonsecamiguel0069@usp.br>
2024-10-09 16:39:47 -0300

    primeiro-trabalho

---
+++ main.c
@@ -0,0 +1,17 @@
+/*******************************************************************
+ * @brief   Trabalho Pratico 1 - Estruturas de dados 3
+ * @authors Miguel Rodrigues Fonseca, Bruno Calabria Cortez Navas
+ * @date    02/07/2024
+ * @note    Trabalho de ED3, quarto periodo, USP Sao Carlos
+ *******************************************************************/
+#include "./funcoesCriadas.h"
+
+int main(){
+
+    int comando;                //variavel de comando
+
+    scanf("%d",&comando);       //leitura do comando que sera executado
+    direcionaComando(comando);  //direciona o programa para cada funcao de acordo com o nro de comando
+
+    return 0;
+}
